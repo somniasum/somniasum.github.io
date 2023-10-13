ccb25ee0dc836c867c3a71e8a5283a0d18665180
zhad <79117272+somniasum@users.noreply.github.com>
2023-10-13 14:09:27 +0200

    console colors

--- console.js
+++ console.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+consoleText(['Hi there, Zhad here.', '...cool right?', 'Short summary on who I am.'], 'text',['white','grey','white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
