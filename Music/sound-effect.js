4864609d018ee44857671e8d4ed380b983a20ad7
zhad <79117272+somniasum@users.noreply.github.com>
2024-06-09 18:22:38 +0200

    Delete Music directory

--- Music/sound-effect.js
+++
@@ -1,8 +0,0 @@
-let more = document.querySelector('.more');
-
-let audioArr = document.getElementsByTagName('audio');
-console.log(audioArr);
-
-more.addEventListener('mouseenter', ()=>{
-	audioArr[0].play();
-});
